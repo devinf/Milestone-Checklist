3bd6f8182d6b1cdc4beddf99858ace1e20add14f
Devin Fan <dfan@boomboxinc.com>
2015-07-13 12:18:41 -0400

    adding folder files pages

---
+++ pages/milestonechecklist.php
@@ -0,0 +1,5 @@
+<?php
+	echo '<p>Here is a link to <a href ="', plugin_page('milestonechecklist'), '">page foo</a>.</p>';
+	echo '<link rel="stylesheet" type="text/css" href="', plugin_file( 'milestonechecklist.css' ), '"/>',
+	'<p class="foo">This is red text</p>';
+?>
